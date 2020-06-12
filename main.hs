74b985a994e986ae43ba4034104a6c4ae4cb7d0a
Johanna Sommer <johanna@mail-sommer.com>
2020-06-12 12:05:02 +0000

    exercise 1 solutions

--- main.hs
+++ main.hs
@@ -25,3 +25,34 @@ argMax' g n m | n > 0 = argMax' g (n-1) temp
 
 g :: Integer -> Integer
 g n = if n < 10 then n * n else n
+
+myPair :: Integer -> Integer -> Integer
+myPair x y = 2 ^ x * (2 * y + 1)
+
+myFst :: Integer -> Integer
+myFst x 
+  | x `mod` 2 == 0 = 1 + myFst (x `div` 2) 
+  | otherwise = 0
+
+mySnd :: Integer -> Integer
+mySnd x = (x `div` 2^(myFst x) - 1) `div` 2
+
+equivMod :: Integer -> Integer -> Integer -> Bool
+equivMod n a b = a == mod b n
+
+quadRes :: Integer -> Integer -> Bool
+quadRes n a = quadResAux n a (n `div` 2)
+  where
+    quadResAux _ _ 0 = equivMod n a 0
+    quadResAux n a x = equivMod n a (x^2) || quadResAux n a (x - 1)
+
+
+prime :: Integer -> Bool
+prime n = prime' n 2
+
+prime' :: Integer -> Integer -> Bool
+prime' n i 
+  | i >= n = True
+  | mod n i == 0 = False
+  | otherwise = prime' n (i+1)
+  
