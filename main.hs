ae86c73c1123d0fa046f70fa829663f65015982f
Johanna Sommer <johanna@mail-sommer.com>
2020-06-12 09:14:54 +0000

    setup, exercise 1

---
+++ main.hs
@@ -0,0 +1,41 @@
+module Exercise_1 where
+
+import Test.QuickCheck
+
+{-H1.1-}
+myPair :: Integer -> Integer -> Integer
+myPair = undefined
+
+{-H1.2-}
+myFst :: Integer -> Integer
+myFst = undefined
+
+{-H1.3-}
+mySnd :: Integer -> Integer
+mySnd = undefined
+
+{-H1.4-}
+prop_myPair :: Integer -> Integer -> Property
+prop_myPair = undefined
+
+{-H2.1-}
+equivMod :: Integer -> Integer -> Integer -> Bool
+equivMod = undefined 
+
+{-H2.2-}
+quadRes :: Integer -> Integer -> Bool
+{-WETT-}
+quadRes = undefined
+{-TTEW-}
+
+{-H2.3-}
+legendre :: Integer -> Integer -> Integer
+legendre = undefined
+
+{-H2.4-}
+prime :: Integer -> Bool
+prime = undefined
+
+{-H2.5-}
+prop_eulersCrit :: Integer -> Integer -> Property
+prop_eulersCrit = undefined
