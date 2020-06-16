6ee452a4974b60bc0b318d4408ad785a02a26286
Johanna Sommer <johanna@mail-sommer.com>
2020-06-16 09:57:39 +0000

    tutorial 2 solutions

--- main.hs
+++ main.hs
@@ -1,58 +1,35 @@
-threeAscending :: Integer -> Integer -> Integer -> Bool
-threeAscending x y z = x < y && y < z
+allSums :: [Integer] -> [Integer] -> [Integer]
+allSums xs ys = [x + y | x <- xs, y <- ys]
 
-fourEqual :: Integer -> Integer -> Integer -> Integer -> Bool
-fourEqual u v w x = u == v && v == w && w == x
+evens :: [Integer] -> [Integer]
+evens xs = [x | x <- xs, even x]
 
-fac :: Integer -> Integer
-fac 1 = 1
-fac n | n > 0 = n * fac (n - 1)
+nLists :: [Integer] -> [[Integer]]
+nLists xs = [[1 .. x] | x <- xs]
 
-sumEleven :: Integer -> Integer
-sumEleven n = sumEleven' n 10
+allEvenSumLists :: [ Integer ] -> [ Integer ] -> [[ Integer ]]
+allEvenSumLists xs ys = nLists (evens (allSums xs ys))
 
-sumEleven' :: Integer -> Integer -> Integer
-sumEleven' n 0 = n
-sumEleven' n i = n + i + sumEleven' n (i - 1)
+toSet :: [Integer] -> [Integer]
+toSet [] = []
+toSet xs
+  | elem (head xs) (tail xs) = toSet (tail xs)
+  | otherwise = [head xs] ++ toSet (tail xs)
 
-argMax :: ( Integer -> Integer ) -> Integer -> Integer
-argMax g n = argMax' g n 0
+isSet :: [Integer] -> Bool
+isSet xs = length xs == (length $ toSet xs)
 
-argMax' :: (Integer -> Integer) -> Integer -> Integer -> Integer
-argMax' g 0 m = m
-argMax' g n m | n > 0 = argMax' g (n-1) temp
-  where temp = if g n > g m then n else m
+union :: [Integer] -> [Integer] -> [Integer]
+union xs ys = toSet $ xs ++ ys
 
-g :: Integer -> Integer
-g n = if n < 10 then n * n else n
+intersection :: [Integer] -> [Integer] -> [Integer]
+intersection xs ys = toSet [x | x <- xs ++ ys, elem x xs, elem x ys]
 
-myPair :: Integer -> Integer -> Integer
-myPair x y = 2 ^ x * (2 * y + 1)
+diff :: [Integer] -> [Integer] -> [Integer]
+diff xs ys = [x | x <- xs, not $ elem x ys]
 
-myFst :: Integer -> Integer
-myFst x 
-  | x `mod` 2 == 0 = 1 + myFst (x `div` 2) 
-  | otherwise = 0
-
-mySnd :: Integer -> Integer
-mySnd x = (x `div` 2^(myFst x) - 1) `div` 2
-
-equivMod :: Integer -> Integer -> Integer -> Bool
-equivMod n a b = a == mod b n
-
-quadRes :: Integer -> Integer -> Bool
-quadRes n a = quadResAux n a (n `div` 2)
-  where
-    quadResAux _ _ 0 = equivMod n a 0
-    quadResAux n a x = equivMod n a (x^2) || quadResAux n a (x - 1)
+eqFrac :: (Integer,Integer) -> (Integer,Integer) -> Bool
+eqFrac (a,b) (c,d) = a*d == c * b
 
 
-prime :: Integer -> Bool
-prime n = prime' n 2
-
-prime' :: Integer -> Integer -> Bool
-prime' n i 
-  | i >= n = True
-  | mod n i == 0 = False
-  | otherwise = prime' n (i+1)
-  
+ 
