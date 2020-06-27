20709caf675bc2586ecb14014696a846414551ea
Johanna Sommer <johanna@mail-sommer.com>
2020-06-27 10:56:36 +0200

    proof sumTree

--- Reps/Rep3_hw.hs
+++ Reps/Rep3_hw.hs
@@ -69,4 +69,81 @@ Proof by induction on List xs
  QED
 
 -------
+data List a = [] | a : List a
+data Tree a = Leaf | Node (Tree a) a (Tree a)
 
+sumTree Leaf = 0
+sumTree (Node l x r) = sumTree l + x + sumTree r
+
+sum [] = 0
+sum (x:xs) = x + sum xs
+
+inorder Leaf = []
+inorder (Node l x r) = (inorder l) ++ (x : (inorder r))
+
+[] ++ ys = ys
+(x:xs) ++ ys = x : (xs ++ ys)
+
+axiom zeroAdd: 0 + b .=. b
+axiom addAssoc: a + (b + c) .=. (a + b) + c
+
+goal sum (inorder t) .=. sumTree t
+
+Lemma sum_append: sum (xs ++ ys) .=. sum xs + sum ys
+Proof by induction on List xs
+
+Case []
+  To show: sum ([] ++ ys) .=. sum [] + sum ys
+
+  Proof
+                              sum ([] ++ ys)
+      (by def ++)         .=. sum ys
+      (by zeroAdd)        .=. 0 + sum ys
+      (by def sum)        .=. sum [] + sum ys
+  QED
+
+Case x:xs
+  To show: sum ((x:xs) ++ ys) .=. sum (x:xs) + sum ys
+  IH:      sum (xs ++ ys)     .=. sum xs + sum ys
+
+  Proof
+                              sum ((x:xs) ++ ys)
+      (by def ++)         .=. sum (x : (xs ++ ys))
+      (by def sum)        .=. x + sum (xs ++ ys)
+      (by IH)             .=. x + (sum xs + sum ys)
+      (by addAssoc)       .=. (x + sum xs) + sum ys
+      (by def sum)        .=. sum (x:xs) + sum ys
+  QED
+QED
+
+Lemma: sum (inorder t) .=. sumTree t
+Proof by induction on Tree t
+Case Leaf
+    To show: sum (inorder Leaf) .=. sumTree Leaf
+
+    Proof
+    sum (inorder Leaf)
+    (by def inorder) .=. sum []
+    (by def sum) .=. 0
+    sumTree Leaf
+    (by def sumTree) .=. 0
+    QED
+
+Case Node l x r
+     To show: sum (inorder (Node l x r)) .=. sumTree (Node l x r)
+       IH1:     sum (inorder l)            .=. sumTree l
+       IH2:     sum (inorder r)            .=. sumTree r
+
+     Proof
+         sum (inorder (Node l x r))
+         (by def inorder) .=. sum ((inorder l) ++ (x : (inorder r)))
+         (by sum_append) .=. sum (inorder l) + sum (x : (inorder r))
+         (by def sum) .=. sum (inorder l) + (x + sum(inorder r))
+         (by IH1) .=. sumTree l + (x + sum(inorder r))
+         (by IH2) .=. sumTree l + (x + sumTree r)
+         (by addAssoc) .=. sumTree l + x + sumTree r
+
+     sumTree (Node l x r)
+         (by def sumTree) .=. sumTree l + x + sumTree r
+     QED
+ QED
